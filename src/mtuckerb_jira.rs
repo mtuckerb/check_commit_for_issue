e995454956f568b514bc051c881d0054c82cbe60
M. Tucker Bradford <tucker@tuckerbradford.com>
2023-04-11 10:59:48 -0400

    refactored jira module to use match blocks

--- src/mtuckerb_jira.rs
+++ src/mtuckerb_jira.rs
@@ -18,25 +18,26 @@ pub async fn lookup_issue(
     auth_token: &str,
     config: &get_config::MtuckerbConfig,
 ) -> Result<bool, String> {
-    let sprint = reqwest::Client::new()
-        .get(format!(
-            "https://{}.atlassian.net/rest/agile/1.0/board/{}/sprint?state=active",
-            config.subdomain, config.board_id
-        ))
-        .header("Content-Type", "application/json")
-        .header("Authorization", format!("Basic {}", &auth_token))
-        .send()
-        .await
-        .expect("Could not connect to Jira")
-        .json::<serde_json::Value>()
-        .await;
-
-    let sprint_id = match sprint {
-        Ok(val) => val["values"][0]["id"].to_string(),
-        Err(_) => "Couldn't find sprint_id".to_string(),
+    let sprint_id = match reqwest::Client::new()
+    .get(format!(
+        "https://{}.atlassian.net/rest/agile/1.0/board/{}/sprint?state=active",
+        config.subdomain, config.board_id
+    ))
+    .header("Content-Type", "application/json")
+    .header("Authorization", format!("Basic {}", &auth_token))
+    .send()
+    .await {
+        Ok(resp) => { 
+            match resp.json::<serde_json::Value>().await {
+                Ok(val) => val["values"][0]["id"].to_string(),
+                Err(_) => "Couldn't find sprint_id".to_string(),
+            }
+        },
+        Err(e) => panic!("Could not connect to Jira: {}", e),
     };
 
-    let issues = reqwest::Client::new()
+
+    let issues = match reqwest::Client::new()
         .get(format!(
             "https://{}.atlassian.net/rest/agile/1.0/board/{}/sprint/{}/issue?maxResults=1000",
             config.subdomain, config.board_id, sprint_id
@@ -44,14 +45,19 @@ pub async fn lookup_issue(
         .header("Content-Type", "application/json")
         .header("Authorization", format!("Basic {}", auth_token))
         .send()
-        .await
-        .expect("Error fetching Issues")
-        .json::<Obj>()
-        .await;
-
+        .await {
+            Ok(resp) => {
+                match resp.json::<Obj>().await {
+                    Ok(data) => data,
+                    Err(e) => panic!("Error parsing response body: {}", e),
+                }
+            }
+            Err(e) => panic!("Error fetching Issues: {}", e),
+        };
+        
     let mut result: bool = false;
 
-    for i in &issues.unwrap().issues {
+    for i in &issues.issues {
         let _ = set_redis(&i.key);
         if i.key.eq(message_id) {
             println!("✅ {:#?} found in Jira!", &i.key);
